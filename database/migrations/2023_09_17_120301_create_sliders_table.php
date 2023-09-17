3712c961d249fef2b97cbd4e223a5d745a9cfc1b
WasikBinMashuk <haquepronoy@gmail.com>
2023-09-17 18:19:33 +0600

    slider migration file added

---
+++ database/migrations/2023_09_17_120301_create_sliders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('slider_title');
+            $table->text('slider_image');
+            $table->string('slider_link');
+            $table->tinyInteger('status')->default(0)->comment('0 = inactive, 1 = active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
