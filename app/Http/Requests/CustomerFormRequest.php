c5e0a91fadc90b1e92ae03cb64efd7c2807be03c
WasikBinMashuk <haquepronoy@gmail.com>
2023-10-23 18:02:17 +0600

    seeder files modified

--- app/Http/Requests/CustomerFormRequest.php
+++ app/Http/Requests/CustomerFormRequest.php
@@ -24,7 +24,7 @@ class CustomerFormRequest extends FormRequest
 
         return [
             'name' => 'required|string|min:1|max:20',
-            'email' => 'required|email|min:1|max:100|unique:customers',
+            'email' => 'required|string|email|min:1|max:100|unique:customers',
             'password' => 'required|string|confirmed|min:6',
             'mobile' => 'required|numeric|digits:11',
         ];
